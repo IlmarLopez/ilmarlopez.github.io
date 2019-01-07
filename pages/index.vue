78cf97bbdf6692b7360bac7b63c88c75b559e7c1
Ilmar López <ilmarlopezr@hotmail.com>
2019-01-06 20:25:39 -0800

    Mensaje de bienvenida.

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,21 @@
 <template>
   <section class="container">
-    <div>
+    <header class="header">
       <logo/>
-      <h1 class="title">
-        ilmarlopez.com
-      </h1>
-      <h2 class="subtitle">
-        &#34;Official website&#34;
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
+    </header>
+    <div class="wrapper">
+      <div class="wrapper-content">
+        <h1 class="welcome-text">
+          coming soon
+        </h1>
+        <p class="welcome-subtext">
+          My new website will launch very soon.
+          <br>
+          Stay tuned!
+        </p>
       </div>
     </div>
+    <!-- <div class="social-icons"></div> -->
   </section>
 </template>
 
@@ -41,25 +38,72 @@ export default {
   text-align: center;
 }
 
-.title {
+.header {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  /* height: 50px; */
+  overflow: hidden;
+}
+
+.header .logo {
+  float: left;
+}
+
+.wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+}
+
+.wrapper-content {
+  padding: 20px;
+  text-align: center;
+  animation: dynamic-fade 1s linear forwards 1s;
+  opacity: 0;
+  margin-top: -30px;
+}
+
+.welcome-text {
   font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
     'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
   display: block;
-  font-weight: 300;
-  font-size: 100px;
+  font-weight: 600;
+  font-size: 50px;
   color: #35495e;
   letter-spacing: 1px;
+  margin-bottom: 30px;
+  text-shadow: 0px 10px 10px rgba(0, 0, 0, 0.4);
+  text-transform: uppercase;
 }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+.welcome-subtext {
+  font-size: 14px;
+  line-height: 24px;
+  letter-spacing: 4px;
+  width: 280px;
+  text-shadow: 0px 2px 3px rgba(0, 0, 0, 0.3), 0px 5px 10px rgba(0, 0, 0, 0.5);
+  text-transform: uppercase;
+  margin: 0 auto;
 }
 
-.links {
-  padding-top: 15px;
+@keyframes dynamic-fade {
+  100% {
+    opacity: 1;
+    margin-top: 0;
+  }
+  80% {
+    margin-top: -5;
+  }
+  40% {
+    margin-top: -15;
+  }
+  10% {
+    opacity: 1;
+    margin-top: -25;
+  }
 }
 </style>
