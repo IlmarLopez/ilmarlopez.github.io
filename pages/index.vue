87abad1a4eeb7ef66085cf7429885b6b4cb11d9b
Ilmar Lopez <ilmarlopezr@hotmail.com>
2022-10-28 22:49:28 -0700

    fix: add components

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Welcome 1</h1>
+  </div>
+</template>
+
+<script>
+// This will work in both `<script setup>` and `<script>`
+definePageMeta({
+  layout: "default",
+});
+</script>
