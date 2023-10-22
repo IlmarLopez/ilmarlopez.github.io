35d8e2f9b89b5f7bd89d916dd36b3dcc2d970ed4
Ilmar Lopez <ilmarlopezr@hotmail.com>
2023-10-22 14:02:32 -0700

    fix: change api

--- plugins/axios.ts
+++ plugins/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 export default defineNuxtPlugin(async () => {
 
   let api = axios.create({
-    baseURL: 'https://65354a23dbcf3a000719b57a--flourishing-valkyrie-ddb50c.netlify.app/',
+    baseURL: 'https://api.ilmarlopez.com/',
     timeout: 1000,
     headers: {
       common: {},
