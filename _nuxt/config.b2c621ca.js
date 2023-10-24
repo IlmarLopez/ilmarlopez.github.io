b893db246625afcd646dc61da5e92ee2fc681295
IlmarLopez <IlmarLopez@users.noreply.github.com>
2023-10-24 18:22:26 +0000

    deploy: d27b4311f25ec6571e634f9218ebcb483ad3f2e5

--- _nuxt/config.b2c621ca.js
+++
@@ -1,1 +0,0 @@
-import{a8 as p,a9 as o,k as i}from"./entry.1af40e9b.js";const a={nuxt:{}},t=p(a);function s(){const n=i();return n._appConfig||(n._appConfig=o(t)),n._appConfig}export{s as u};
---
+++ _nuxt/config.f6b37db1.js
@@ -0,0 +1,1 @@
+import{a8 as p,a9 as o,k as i}from"./entry.4c2859e6.js";const a={nuxt:{}},t=p(a);function s(){const n=i();return n._appConfig||(n._appConfig=o(t)),n._appConfig}export{s as u};
